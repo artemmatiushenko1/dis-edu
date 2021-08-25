35e328f102820f66f432276b6b35dafbd1122733
boldak <boldak.andrey@gmail.com>
2021-08-25 23:36:11 +0300

    initial

---
+++ ReadMe.md
@@ -0,0 +1,52 @@
+---
+home: true
+actionText: Get Started →
+actionLink: /custom-components/
+features:
+- title: Admonitions and Tables
+  details: Uses google material icons and sortable tables
+- title: Charts and Math
+  details: Leverage chartjs and katex 
+- title: Diagramming tools
+  details: Integrated with mermaid and plantuml
+footer: MIT Licensed | Copyright © 2020-present David Li
+---
+
+
+## Disclaimer
+
+As of `vuepress@1.7.1` I cannot get this starter template running with vuepress installed globally, it throws an error message for me.
+
+
+## ReadMe
+
+This is an example of how to use the `vuepress-theme-cool` vuepress theme, look into docs for example files.
+
+## Installation and Running
+
+To install:
+
+`npm install`
+
+To run with vuepress installed locally:
+
+`npm install vuepress --save-dev`
+
+`npm run docs:dev`
+
+
+## Customization
+
+Most of the customization is done in `config.js`, automatic navbar and sidebar navigation is available in `config-other.js`
+
+Some other good vuepress examples can be found at [vuepress-examples](https://vuepress-examples.netlify.com/)
+
+
+
+If you would like to motivate me to spend more time improving open source projects please consider donating.
+
+[![Donate with Ethereum](https://en.cryptobadges.io/badge/big/0x9d18acAB9Fe749Cbf899B2FD63Bf25e64829bbF3)](https://en.cryptobadges.io/donate/0x9d18acAB9Fe749Cbf899B2FD63Bf25e64829bbF3)
+
+[![Donate with Bitcoin](https://en.cryptobadges.io/badge/big/1BMWhjCrTE3Dn94oHnrk6XMZAS3hjq3vdD)](https://en.cryptobadges.io/donate/1BMWhjCrTE3Dn94oHnrk6XMZAS3hjq3vdD)
+
+[![paypal](https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=Z6M6Y83D3URSU&item_name=Motivating+me+to+continue+to+produce+open+source+projects&currency_code=CAD)
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,127 @@
+// .vuepress/config.js
+module.exports = {
+  plugins: [
+    '@vuepress/back-to-top', 
+    '@vuepress/pwa', 
+    {
+      serviceWorker: true,
+      updatePopup: true
+    },
+    ,
+    ['container', {
+      type: 'vue',
+      before: '<pre class="vue-container"><code>',
+      after: '</code></pre>'
+    }]
+  ],
+  port: 8084,
+  base: '/dis-edu/',
+  theme: 'cool',
+  dest: 'dist',
+  head: [
+    ['link', { rel: 'icon', href: '/faviconCustom.ico' }],
+    ['link', { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css' }],
+    ['link', {href: 'https://fonts.googleapis.com/icon?family=Material+Icons', rel :'stylesheet'}],
+    
+  ],
+  themeConfig: {
+    // logo: './myAvatar.png',
+    sidebar: [
+    {
+      title: 'Вступ',
+      path:"/intro/",
+      children:[
+        '/intro/10.distributed--information-system',
+      ]
+    },
+    {
+      title: 'Архітетура розподілених інформаційних систем',
+      path:"/arch/",
+      children:[
+        '/arch/11.arch',
+      ]
+    },
+
+    {
+      title: 'Реалізація взаємодії компонентів розподіленої інформаційної системи',
+      path:"/components-interaction/",
+      children:[
+        '/components-interaction/01.express',
+        '/components-interaction/03.restfull-services',
+        '/components-interaction/04.graphql',
+        '/components-interaction/05.sse',
+        '/components-interaction/06.scripted-query'
+         
+      ]
+    },
+
+    {
+      title: 'Прикладний програмний інтерфейс сервісів',
+      path:"/swagger/",
+      children:[
+        '/swagger/12.api' 
+      ]
+    },
+
+    {
+      title: 'Реалізація взаємодії компонентів в EDA',
+      path:"/eda/",
+      children:[
+        '/eda/13.rabbitmq'
+      ]
+    },
+
+    {
+      title: 'Реалізація взаємодії сервісів в MSA',
+      path:"/msa/",
+      children:[
+        '/msa/14.msa'
+      ]
+    }
+  ],
+    sidebarDepth: 2,
+    displayAllHeaders: true, // Default: false
+
+
+    nav: [
+      { text: 'Home', link: '/' },
+    ],
+    lastUpdated: 'Last Updated', // string | boolean
+      // Assumes GitHub. Can also be a full GitLab url.
+    // repo: 'FriendlyUser/vuepress-theme-cool-starter',
+    // Customising the header label
+    // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
+    // repoLabel: 'Github',
+
+    // Optional options for generating "Edit this page" link
+
+    // if your docs are in a different repo from your main project:
+    docsRepo: 'boldak/dis-edu',
+    // if your docs are not at the root of the repo:
+    docsDir: 'docs',
+    // if your docs are in a specific branch (defaults to 'master'):
+    docsBranch: 'master',
+    // defaults to false, set to true to enable
+    editLinks: true,
+    // custom text for edit link. Defaults to "Edit this page"
+    editLinkText: 'Help us improve this page!'
+
+  },
+  title: 'Розподілені інформаційні системи',
+  description: 'Курс лекцій',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@alias': '../img'
+      }
+    }
+  },
+  markdown: {
+    extendMarkdown: md => {
+      md.set({ html: true })
+      md.use(require('markdown-it-katex'))
+      md.use(require('markdown-it-plantuml'))
+      md.use(require('markdown-it-admonition'))
+    }
+  }
+}
